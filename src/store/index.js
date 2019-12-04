04a397efd5b850ef3f8f2f5fa0b01743e8682c21
BK1917xuexusheng <3119455967@qq.com>
2019-12-04 09:17:32 +0800

    1204合并master

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import { createStore, combineReducers, applyMiddleware } from "redux"
-import reduxThunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import home from "./reducers/home";
-import register from "./reducers/register";
-import login from "./reducers/login"
-const reducers = combineReducers({
-    home,
-    register,
-    login
-})
-
-
-
-
-const store = createStore(reducers, applyMiddleware(reduxThunk));
-=======
 import {createStore,combineReducers,applyMiddleware} from "redux";
 import reduxThunk from "redux-thunk";
 import {composeWithDevTools} from "redux-devtools-extension"
@@ -24,18 +6,22 @@ import smallMBook from "./reducers/smallMBook"
 import goodList from "./reducers/goodList"
 import details from "./reducers/details"
 import brand from "./reducers/brand"
-
+import home from "./reducers/home";
+import register from "./reducers/register";
+import login from "./reducers/login"
 
 const reducers = combineReducers({
     classify,
     smallMBook,
     goodList,
     details,
-    brand
+    brand,
+    home,
+    register,
+    login
 })
 
 const store = createStore(reducers,composeWithDevTools(applyMiddleware(reduxThunk)))
->>>>>>> d484ee9a5a8c065a0babab07fb838b0a27c0caa7
 
 
 export default store;
